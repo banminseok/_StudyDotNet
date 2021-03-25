72f0d5e070f3177fd5e3438265cce72e8bbdc07a
ban <halfmin@gmail.com>
2021-03-25 16:59:42 +0900

    asp-page Test

--- DotNetStudy/RouteTest/Startup.cs
+++ DotNetStudy/RouteTest/Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RouteTest
 {
@@ -51,6 +46,10 @@ namespace RouteTest
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
